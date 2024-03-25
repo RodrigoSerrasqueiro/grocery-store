019826197013553809d8cb046028da14d43fbebf
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2024-03-25 16:35:52 -0300

    feat: create Header component

--- app/page.js
+++ app/page.js
@@ -2,10 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <h2>Hello, world!</h2>
-      <Button>Iniciar</Button>
-    </main>
-  );
+  return <main>Home</main>;
 }
