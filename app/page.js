dde35929648f32ceffd8e195f227ecb8e1632741
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2024-07-26 16:09:32 -0300

    fix: banner height

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export default async function Home() {
         alt="banner"
         width={1000}
         height={300}
-        className="w-full h-[400px] mt-5"
+        className="w-full h-[170px] md:h-[400px] mt-5"
       />
       <Footer />
     </main>
