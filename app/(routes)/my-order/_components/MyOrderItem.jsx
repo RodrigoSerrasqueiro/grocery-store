fe4c30e9f795950ad3304390b1a7e00722700146
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2024-08-07 18:08:44 -0300

    fix: nextjs.config

--- app/(routes)/my-order/_components/MyOrderItem.jsx
+++ app/(routes)/my-order/_components/MyOrderItem.jsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 
 function OrderItemComponent({ orderItem }) {
-  console.log(orderItem)
   return (
     <div className="w-full md:w-[60%]">
       <div className="grid grid-cols-2 md:grid-cols-5 mt-3 items-center">
@@ -11,6 +10,7 @@ function OrderItemComponent({ orderItem }) {
           width={80}
           height={80}
           className="bg-gray-100 p-5 rounded-md border w-[80px] h-[80px]"
+          unoptimized
         />
         <div className="md:col-span-2">
           <h2>{orderItem.product.data.attributes.name}</h2>
